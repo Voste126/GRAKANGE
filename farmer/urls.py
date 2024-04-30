09d9a6953d2e84aa29e29375bbdef70d2ab3f827
voste126 <steveaustine126@gmail.com>
2024-04-30 14:22:37 +0300

    DJANGO RESTFUL API

--- farmer/urls.py
+++ farmer/urls.py
@@ -1,19 +1,3 @@
-# """
-# URL configuration for farmer project.
-
-# The `urlpatterns` list routes URLs to views. For more information please see:
-#     https://docs.djangoproject.com/en/5.0/topics/http/urls/
-# Examples:
-# Function views
-#     1. Add an import:  from my_app import views
-#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
-# Class-based views
-#     1. Add an import:  from other_app.views import Home
-#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-# Including another URLconf
-#     1. Import the include() function: from django.urls import include, path
-#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-# """
 # from django.contrib import admin
 # from django.urls import path, include
 # from rest_framework.routers import DefaultRouter
@@ -22,6 +6,22 @@
 # from drf_yasg import openapi
 # from drf_yasg.views import get_schema_view as swagger_get_schema_view
 
+# # """
+# # URL configuration for farmer project.
+
+# # The `urlpatterns` list routes URLs to views. For more information please see:
+# #     https://docs.djangoproject.com/en/5.0/topics/http/urls/
+# # Examples:
+# # Function views
+# #     1. Add an import:  from my_app import views
+# #     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# # Class-based views
+# #     1. Add an import:  from other_app.views import Home
+# #     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# # Including another URLconf
+# #     1. Import the include() function: from django.urls import include, path
+# #     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# # """
 # schema_view = swagger_get_schema_view(
 #     openapi.Info(
 #         title= "Grakange API",
@@ -42,12 +42,10 @@
 #     path('admin/', admin.site.urls),
 #     path('', include('users.urls')),
 #     path('',include('retailers.urls')),
-#     path('', include([
-#         path('', include(router.urls)),
-#         path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-#     ])),
+#     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('', include(router.urls)),
 # ]
-from django.contrib import admin
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import FarmerViewSet, FarmViewSet
@@ -72,7 +70,5 @@ router.register(r'business', BusinessViewSet, basename='business')
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='docs-swagger-ui'),  # Changed the name to avoid conflict
-    path('api/', include(router.urls)),
+    path('docs/', include(router.urls)),
 ]
